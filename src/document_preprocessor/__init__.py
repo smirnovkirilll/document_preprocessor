723bda07d228cb5af4ab27fe7cc10e4b68f02bc9
kirill <kirillturkey7497@gmail.com>
2025-12-27 00:40:18 +0300

    pyproject fixes

--- src/document_preprocessor/__init__.py
+++ src/document_preprocessor/__init__.py
@@ -1,4 +1,5 @@
+__version__ = "2025.12.26.2"
+
 from .core import PreprocessorConfig, DocumentPreprocessor, DocumentProcessingError
 
 __all__ = ["PreprocessorConfig", "DocumentPreprocessor", "DocumentProcessingError"]
-__version__ = "2025.12.26"
